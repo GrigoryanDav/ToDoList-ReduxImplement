fa3b20391e19870935b8fde223613e1428f7bf83
GrigoryanDav <grigoryand219@gmail.com>
2024-11-01 17:26:49 +0400

    Implement of actions functionality

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import { ADD_TODO, REMOVE_TODO } from "../utils/constants";
+
+
+export const addTodo = (store, task) => {
+    store.dispatch({
+        type: ADD_TODO,
+        payload: {'id': Date.now(), task}
+    })
+}
+
+export const removeTodo = (store, id) => {
+    store.dispatch({
+        type: REMOVE_TODO,
+        payload: id,
+    })
+}
