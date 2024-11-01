3c24b29320f163c958a689c73edb31c55989bd7d
GrigoryanDav <grigoryand219@gmail.com>
2024-11-01 17:39:23 +0400

    Add todo list rendering function

---
+++ src/components/ToDoList/index.js
@@ -0,0 +1,21 @@
+import { removeTodo } from "../../actions";
+
+
+export const ToDoList = (store) => {
+    const todoListContainer = document.getElementById('todo-list')
+
+    return () => {
+        const todos = store.getState().todos
+        todoListContainer.innerHTML = ''
+
+        todos.forEach(todo => {
+            const listItem = document.createElement('li')
+            listItem.textContent = todo.task
+            const deleteButton = document.createElement('button')
+            deleteButton.textContent = 'Delete'
+            deleteButton.onclick = () => removeTodo(store, todo.id)
+            listItem.appendChild(deleteButton)
+            todoListContainer.appendChild(listItem)
+        });
+    }
+};
